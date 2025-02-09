85a93edf7647a4d91b3d480f5e804835e44e597e
sModsk <smod01sk@gmail.com>
2025-02-09 20:14:18 +0200

    ESX job fix, QBCore job fix

--- client/bridge/bridge_job.lua
+++ client/bridge/bridge_job.lua
@@ -6,12 +6,15 @@ end)
 RegisterNetEvent('esx:playerLoaded',function(xPlayer, isNew, skin)
     job = xPlayer.job
 end)
-CreateThread(function () Wait(100) job = ESX.PlayerData.job end)
+CreateThread(function () Wait(100) if ESX then job = ESX.PlayerData.job end end)
 
 function GetJob()
     if ESX then 
         return job
     else
-        return QBCore.Functions.GetPlayerData().job
+        local _job = QBCore.Functions.GetPlayerData().job
+        job.name = _job.name
+        job.grade = _job.grade.level
+        return job
     end
 end
